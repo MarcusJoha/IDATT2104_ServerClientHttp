9747f9e758f346622f186752c8e4b04b98fd6542
Marcus Johannessen <marcusjo@stud.ntnu.no>
2022-02-09 12:05:11 +0100

    finished Client

--- src/oblig4/Client.java
+++ src/oblig4/Client.java
@@ -1,2 +1,50 @@
-package oblig4;public class Client {
+package oblig4;
+
+/*
+Client for UDP server
+ */
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+        try {
+
+            InetAddress address = InetAddress.getLocalHost();
+            DatagramSocket socket = new DatagramSocket();
+            Scanner sc = new Scanner(System.in);
+            String receivedFromServer;
+            String equation;
+
+            do {
+
+                // Send and equation to Server (UDP connection)
+                System.out.print("Write an equation: ");
+                equation = sc.nextLine();
+
+                byte[] buffer = equation.getBytes();
+                DatagramPacket packet = new DatagramPacket(buffer, 0, address, 5020);
+                socket.send(packet);
+
+                // receive Answer back from Server
+                byte[] receivedBuffer = new byte[50];
+                packet = new DatagramPacket(receivedBuffer, 0, receivedBuffer.length);
+                socket.receive(packet);
+                receivedFromServer = new String(receivedBuffer,0, packet.getLength());
+                System.out.println("Received from server: " + receivedFromServer);
+
+            } while (!equation.equals(""));
+
+        } catch (SocketTimeoutException e) {
+            System.out.println("Socket timed out: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
--- src/oblig4/Server.java
+++ src/oblig4/Server.java
@@ -1,2 +1,40 @@
-package oblig4;public class Server {
+package oblig4;
+
+/*
+oppg 1. UDP server
+ */
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        try {
+            DatagramSocket socket = new DatagramSocket(5010);
+
+            while (true) {
+                byte[] buffer = new byte[40]; // Burde ha en metode for å utvide buffer her
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+
+                String received = new String(buffer, 0, packet.getLength());
+                System.out.println("Text received is: " + received);
+
+                // Data is now received from Client, and we have to handle it here.
+
+
+            }
+
+        } catch (SocketException e) {
+            System.out.println("Something wron with socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+
+
+    }
 }
